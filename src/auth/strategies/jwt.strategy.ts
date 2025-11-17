1a0741d06de154552ce838e8290ce6c8db00c6be
이현종 <ehdclr@gractor.com>
2025-11-17 14:53:52 +0900

    feat: jwt Strategy 추가

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -27,7 +27,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     return {
       id: payload.sub,
       email: payload.email,
-      name: payload.name
+      name: payload.name,
     };
   }
 }
