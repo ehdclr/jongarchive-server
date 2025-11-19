c2a8485a547e688cb0a458e2368dab8cc2f1ec12
이현종 <ehdclr@gractor.com>
2025-11-19 15:00:56 +0900

    change: package.json scripts 수정, database users 테이블 bigserial수정

--- src/database/schema/user.ts
+++ src/database/schema/user.ts
@@ -1,7 +1,7 @@
-import { pgTable, serial, varchar, text, timestamp, jsonb } from 'drizzle-orm/pg-core';
+import { pgTable, varchar, text, timestamp, bigserial } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
-  id: serial('id').primaryKey(),
+  id: bigserial('id', { mode: 'number' }).primaryKey(), // unsigned biginteger 사용
   socialId: varchar('social_id', { length: 255 }).default(''),
   email: varchar('email', { length: 255 }).notNull().unique(),
   phoneNumber: varchar('phone_number', { length: 255 }).default(''),
