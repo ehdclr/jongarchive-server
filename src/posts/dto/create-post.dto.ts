df28f1aeae70593b16111fbba6a89f5836e941ed
이현종 <ehdclr@gractor.com>
2025-11-25 15:44:39 +0900

    feat: post dto추가

---
+++ src/posts/dto/create-post.dto.ts
@@ -0,0 +1,12 @@
+import { IsString, IsNotEmpty, MaxLength } from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty({ message: '제목을 입력해주세요.' })
+  @MaxLength(255, { message: '제목은 255자 이하로 입력해주세요.' })
+  title: string;
+
+  @IsString()
+  @IsNotEmpty({ message: '내용을 입력해주세요.' })
+  content: string;
+}
